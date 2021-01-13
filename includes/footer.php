b6ca957c5700908c2e41d17f76122deeb359049f
rudkik <66820365+rudkik@users.noreply.github.com>
2021-01-13 10:23:45 +0300

    1#
    Создание структуры

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">dmwdwk</div>
+</body>
+</html>
---
+++ public/admin.php
@@ -0,0 +1,18 @@
+<?php include("../includes/header.php");?>
+<?php require_once("../includes/functions.php");?>
+
+
+<!DOCTYPE html>
+
+    <div id="main">
+        <div id="navigation"></div>
+        <div id="page">
+            <ul>
+                <li><a href="manage_content.php"> Content Website</a> </li>
+                <li><a href="manage_admin.php"> Content  Admins Website</a> </li>
+                <li><a href="logout.php"> logout</a> </li>
+            </ul>
+        </div>
+    </div>
+<?php include("../includes/footer.php");?>
+
