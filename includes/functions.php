18a8209cfb24b0b84fb43c232bd730332f017954
rudkik <66820365+rudkik@users.noreply.github.com>
2021-01-17 12:57:06 +0300

    5#
    провели рефакторин кода в частности навигации сайдабара

--- includes/functions.php
+++ includes/functions.php
@@ -4,6 +4,28 @@
             die("ошибка выдаче сайдбара )");
         }
     }
+    function find_all_subjects(){
+        global $connection;
 
+        $query = "SELECT * ";
+        $query .= "FROM subjects ";
+        $query .= "WHERE visible = 1 ";
+        $subjects_set = mysqli_query($connection, $query);
+        confirm_query($subjects_set);
+        return $subjects_set;
+    }
+
+    function find_pages_for_subject($subjects_id){
+        global $connection;
+
+        $query = "SELECT * ";
+        $query .= "FROM pages ";
+        $query .= "WHERE visible = 1 ";
+        $query .= "AND subject_id = {$subjects_id} ";
+        $page_set = mysqli_query($connection, $query);
+        confirm_query($page_set);
+        return $page_set;
+
+    }
 
 ?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -2,31 +2,22 @@
 <?php include("../includes/header.php");?>
 <?php require_once("../includes/functions.php");?>
 
-<?php
-$query = "SELECT * ";
-$query .= "FROM subjects ";
-$query .= "WHERE visible = 1 ";
-$subjects_set = mysqli_query($connection, $query);
-confirm_query($subjects_set);
-?>
+
 
 <!DOCTYPE html>
 
 <div id="main">
     <div id="navigation">
         <?php
+        $subjects_set = find_all_subjects();
         while ($subjects = mysqli_fetch_assoc($subjects_set)){
             ?>
         <li>
-            <?php echo $subjects['menu_name'] ;?>
             <?php
-            $query = "SELECT * ";
-            $query .= "FROM pages ";
-            $query .= "WHERE visible = 1 ";
-            $query .= "AND subject_id = {$subjects["id"]} ";
-            $page_set = mysqli_query($connection, $query);
-            confirm_query($page_set);
+                echo $subjects['menu_name'];
+                $page_set = find_pages_for_subject($subjects["id"]);
             ?>
+            
             <ul>
                 <?php while ($page = mysqli_fetch_assoc($page_set)){  ?>
                 <li>  <?php echo $page['menu_name'] ;?>  </li>
