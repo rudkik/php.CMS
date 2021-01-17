0d5ced7abfa646276c8982512c891ff82e027f8f
rudkik <66820365+rudkik@users.noreply.github.com>
2021-01-17 13:23:12 +0300

    6#
    Добавили проверку на вывод контента каждой старницы (ссылки) так же сделали выделения чтоб было понятно какая ссылка сейчас активна

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,37 +1,54 @@
 <?php require_once("../includes/db_conection.php");?>
 <?php include("../includes/header.php");?>
 <?php require_once("../includes/functions.php");?>
-
-
+<?php if (isset($_GET["subjects"])){
+    $selected_subjects_id = $_GET["subjects"];
+    $selected_page_id = null;}
+    elseif (isset($_GET["page"])){
+    $selected_page_id = $_GET["page"];
+    $selected_subjects_id = null;
+    }
+    else {
+        $selected_page_id = null;
+        $selected_subjects_id = null;
+    }?>
 
 <!DOCTYPE html>
-
 <div id="main">
     <div id="navigation">
         <?php
         $subjects_set = find_all_subjects();
         while ($subjects = mysqli_fetch_assoc($subjects_set)){
             ?>
-        <li>
             <?php
-                echo $subjects['menu_name'];
-                $page_set = find_pages_for_subject($subjects["id"]);
+            echo "<li" ;
+            if ($selected_subjects_id == $subjects["id"]){
+                echo " style=\"font-size:20px;\"";
+            }
+            echo ">" ;
             ?>
-            
+           <a href="manage_content.php?subjects=<?php echo urlencode($subjects["id"])?>"> <?php echo $subjects['menu_name'];?> </a>
+               <?php $page_set = find_pages_for_subject($subjects["id"]); ?>
+
             <ul>
                 <?php while ($page = mysqli_fetch_assoc($page_set)){  ?>
-                <li>  <?php echo $page['menu_name'] ;?>  </li>
+                    <?php
+                    echo "<li" ;
+                    if ($selected_page_id == $page["id"]){
+                        echo " style=\"font-size:20px;\"";
+                    }
+                    echo ">" ;
+                    ?>  <a href="manage_content.php?page=<?php echo urlencode($page["id"])?>"> <?php echo $page['menu_name'] ;?> </a> </li>
                 <?php } ?>
             </ul>
         </li>
         <?php } ?>
     </div>
     <div id="page">
-        <ul>
-            <li><a href="manage_content.php"> Content Website</a> </li>
-            <li><a href="admin.php"> Content  Admins Website</a> </li>
-            <li><a href="logout.php"> logout</a> </li>
-        </ul>
+        <h2> Мэнедже контент</h2>
+        <?php echo $selected_subjects_id;?>
+        <?php echo $selected_page_id;?>
+
     </div>
 </div>
 <?php include("../includes/footer.php");?>
